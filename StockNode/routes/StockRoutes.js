55af55c8cf6db5f1d3dde9443d90d7ca8b29f79f
prakashsrirangaswamy <rangaswamy.prakash@gmail.com>
2019-11-07 13:33:03 +0530

    BTC api call

--- StockNode/routes/StockRoutes.js
+++ StockNode/routes/StockRoutes.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
+require('dotenv').config()
+const url = "https://www.alphavantage.co/query"
 
-router.post('/value', function (req, res) {
-    console.log('inside value');
-    res.send("inside Stock module");
+router.get('/value', function (req, res) {
+    var url_value = url
+        + "?function=" + req.query.function
+        + "&from_currency=" + req.query.from_currency
+        + "&to_currency=" + req.query.to_currency
+        + "&apikey=" + process.env.API_KEY;
+
+    axios.get(url_value)
+        .then(response => {
+            if (response.status === 200) {
+                res.send(response.data);
+            } else {
+                console.log(response.status)
+            }
+        }).catch(error => { console.log(error); })
 })
 module.exports = router;
