5b3db0975cbdedd50faf6a439ea6b8cfd189fa9b
prakashsrirangaswamy <rangaswamy.prakash@gmail.com>
2019-11-11 12:27:08 +0530

    update for the crypto module

--- StockNode/client/src/components/crypto.js
+++ StockNode/client/src/components/crypto.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-// import Axios from 'axios';
-
-
 export default class crypto extends Component {
     constructor(props) {
         super(props);
@@ -14,47 +11,32 @@ export default class crypto extends Component {
         };
     }
 
-
     async componentDidMount() {
         const url = '/stock/value?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=INR';
-        // free serivce allows only 5 hits per min and 500 hits per day, so mocking the response. 
-        const response = {
-            "Realtime Currency Exchange Rate": {
-                "1. From_Currency Code": "BTC",
-                "2. From_Currency Name": "Bitcoin",
-                "3. To_Currency Code": "INR",
-                "4. To_Currency Name": "Indian Rupee",
-                "5. Exchange Rate": "632479.93450000",
-                "6. Last Refreshed": "2019-11-09 14:01:16",
-                "7. Time Zone": "UTC",
-                "8. Bid Price": "632464.23750000",
-                "9. Ask Price": "632523.45800000"
-            }
-        }; const data = response;
-        // const response = await fetch(url);
-        // const data = await response.json();
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({
+            cryptoData: data['Realtime Currency Exchange Rate'], loading: false,
+            currency: 'INR', crypto: 'BTC'
+        });
+    }
 
-
-        this.setState({ cryptoData: data['Realtime Currency Exchange Rate'], loading: false })
+    async getNodeResponse() {
+        if (null != this.state.currency && null != this.state.crypto) {
+            const url = '/stock/value?function=CURRENCY_EXCHANGE_RATE&from_currency=' + this.state.crypto + '&to_currency=' + this.state.currency;
+            const response = await fetch(url);
+            const data = await response.json();
+            this.setState({ cryptoData: data['Realtime Currency Exchange Rate'] });
+        }
     }
 
     changeDropDownHandler = (event) => {
         if (event.target.name === 'crypto') {
-            this.setState({ crypto: event.target.value });
+            this.setState({ crypto: event.target.value }, () => this.getNodeResponse());
         } else if (event.target.name === 'currency') {
-            this.setState({ currency: event.target.value });
+            this.setState({ currency: event.target.value }, () => this.getNodeResponse());
         }
 
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const url = '/stock/value?function=CURRENCY_EXCHANGE_RATE&from_currency=' + `${this.state.crypto}` + '&to_currency=' + `${this.state.currency}`;
-
-        // Axios.get(url).then(response => {
-        //     alert('inside')
-        //     console.log(response.data);
-        // })
     }
 
     render() {
@@ -67,26 +49,19 @@ export default class crypto extends Component {
                             <div>
                                 <h1>Crypto Tracker</h1>
                                 Crypto:
-                            <select name='crypto' value={crypto} onBlur={this.changeDropDownHandler}>
-                                    <option value='empty'></option>
+                            <select name='crypto' value={crypto} onChange={this.changeDropDownHandler}>
                                     <option value='BTC'>Bitcoin</option>
                                     <option value='LTC'>Litecoin</option>
                                 </select>
                                 <br></br>
                                 Currency:
-                            <select name='currency' value={currency} onBlur={this.changeDropDownHandler}>
-                                    <option value='empty'></option>
+                            <select name='currency' value={currency} onChange={this.changeDropDownHandler}>
                                     <option value='INR'>Indian Rupee</option>
                                     <option value='USD'>US Dollors</option></select>
-
-
                             </div>
-                            <button type="submit" name="submit">Submit</button>
                             <div>A {cryptoData['2. From_Currency Name']} is <b>{cryptoData['3. To_Currency Code']} {cryptoData['5. Exchange Rate']}</b>  as of {cryptoData['6. Last Refreshed']}</div>
-
                         </div> </form>)}
             </div >
-
         )
     };
 
